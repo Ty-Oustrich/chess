01fbd1a82d3f9c611b8a8162f177b646dd3ab000
Tyler Oustrich <oustrichtyler@gmail.com>
2026-02-26 10:03:12 -0700

    added the model files

--- shared/src/main/java/model/AuthData.java
+++ shared/src/main/java/model/AuthData.java
@@ -1,7 +1,9 @@
 package model;
 
+
 public record AuthData(String authToken, String username) {
 }
 
 // links an auth token to its username
 // created by UserService on successful register or login. Is deleted on logout.
+
--- shared/src/main/java/model/UserData.java
+++ shared/src/main/java/model/UserData.java
@@ -1,8 +1,10 @@
 package model;
 
+
 public record UserData(String username, String password, String email) {
 }
 
 //A user
 //used by UserService for registration and login auth
 //Stored and accessed by DataAccess methods
+
