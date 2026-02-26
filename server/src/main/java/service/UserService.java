29456f209e78cc2ebb19a1a965b74c41229c1bee
Tyler Oustrich <oustrichtyler@gmail.com>
2026-02-26 10:46:37 -0700

    userservice.java implemented, but not yet tested

--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -1,7 +1,33 @@
 package service;
 
-public class UserService {
+import dataaccess.DataAccess;
+import dataaccess.DataAccessException;
+import model.AuthData;
+import model.UserData;
+import java.util.UUID;
 
+public class UserService {
+    private final DataAccess dataAccess;
+
+    public UserService(DataAccess dataAccess) {
+        this.dataAccess = dataAccess;
+    }
+
+    public LoginResult login(LoginRequest request) throws DataAccessException {
+        UserData user = dataAccess.getUser(request.username());
+
+        if (user == null) {
+            throw new DataAccessException("Error: username was wrong");
+        }
+        if (!user.password().equals(request.password())) {
+            throw new DataAccessException("Error: password was wrong");
+        }
+
+        String token = UUID.randomUUID().toString();
+        String username = user.username();
+        AuthData authData = new AuthData(token, username);
+        dataAccess.createAuth(authData);
+        
+        return new LoginResult(username, token);
+    }
 }
-//logic for various endpoints validates, manages tokens etc. IM not exactly sure what will
-//be inclued in here
