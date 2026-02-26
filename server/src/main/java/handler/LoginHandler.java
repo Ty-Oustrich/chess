b873e45a2ee018e3d994e9417515733595745174
Tyler Oustrich <oustrichtyler@gmail.com>
2026-02-26 13:48:36 -0700

    LoginHandler, next I will implement ErrorResponse and LoginResult

--- server/src/main/java/handler/LoginHandler.java
+++ server/src/main/java/handler/LoginHandler.java
@@ -1,8 +1,58 @@
 package handler;
 
-public class LoginHandler {
 
+import com.google.gson.Gson;
+import dataaccess.DataAccessException;
+import io.javalin.http.Context;
+import io.javalin.http.Handler;
+import service.LoginRequest;
+import service.UserService;
+import java.util.Map;
+
+public class LoginHandler {
+    private final UserService userService;
+    private final Gson gson = new Gson();
+
+    public LoginHandler(UserService userService) {
+        this.userService = userService;
+    }
     
+    public void handle(Context ctx) {
+        try {
+            LoginRequest request = gson.fromJson(ctx.body(), LoginRequest.class);
+            LoginResult result = userService.login(request);
+            sendSuccess(ctx, result);
+        } catch (DataAccessException e) {
+            sendError(ctx, e);
+        } catch (Exception e) {
+            sendUnexpectedError(ctx, e);
+        }
+    }
+    
+    private void sendSuccess(Context ctx, LoginResult result) {
+        ctx.status(200);
+        ctx.json(result);
+    }
+    
+    private void sendUnexpectedError(Context ctx, Exception e) {
+        String errorMessage = "Error: " + e.getMessage();
+        ErrorResponse errorResponse = new ErrorResponse(errorMessage);
+        ctx.status(500);
+        ctx.json(errorResponse);
+    }
+
+    private void sendError(Context ctx, DataAccessException e) {
+        String errorMessage = e.getMessage();
+        ErrorResponse errorResponse = new ErrorResponse(errorMessage);
+    
+        if (errorMessage.equals("Error: unauthorized")) {
+            ctx.status(401);
+        } else {
+            ctx.status(500);
+        }
+    
+        ctx.json(errorResponse);
+    }
 
 }
 
