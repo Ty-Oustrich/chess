732f9934b9fa1d5b1871ecd52496133a978c686d
Tyler Oustrich <oustrichtyler@gmail.com>
2026-03-05 13:35:50 -0700

    server.java delete route

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,16 +1,22 @@
 package server;
 
+import dataaccess.DataAccess;
+import dataaccess.MemoryDataAccess;
+import handler.ClearHandler;
 import io.javalin.*;
+import service.ClearService;
 
 public class Server {
-
     private final Javalin javalin;
 
     public Server() {
         javalin = Javalin.create(config -> config.staticFiles.add("web"));
 
-        // Register your endpoints and exception handlers here.
+        DataAccess dataAccess = new MemoryDataAccess();
+        ClearService clearService = new ClearService(dataAccess);
+        ClearHandler clearHandler = new ClearHandler(clearService);
 
+        javalin.delete("/db", clearHandler::handle);
     }
 
     public int run(int desiredPort) {
