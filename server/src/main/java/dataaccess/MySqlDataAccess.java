6c97d16b3ea3ee6f863fb57e7499038421b3b28a
Tyler Oustrich <oustrichtyler@gmail.com>
2026-03-11 13:03:20 -0600

    implemented listgames creategame and update getGame to use mapping from readGameFromResultSet(ResultSet)

--- server/src/main/java/dataaccess/MySqlDataAccess.java
+++ server/src/main/java/dataaccess/MySqlDataAccess.java
@@ -10,6 +10,7 @@ import util.GsonFactory;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.Collection;
 
 public class MySqlDataAccess implements DataAccess {
@@ -35,28 +36,7 @@ public class MySqlDataAccess implements DataAccess {
                 return null;
             }
 
-            int storedGameId = resultSet.getInt("game_id");
-            String whiteUsername = resultSet.getString("white_username");
-            String blackUsername = resultSet.getString("black_username");
-            String gameName = resultSet.getString("game_name");
-            String gameStateJson = resultSet.getString("game_state_json");
-
-            ChessGame deserializedGame;
-            try {
-                deserializedGame = gson.fromJson(gameStateJson, ChessGame.class);
-            } catch (JsonSyntaxException exception) {
-                String message = "corrupt game_state_json for game_id=" + storedGameId;
-                throw new DataAccessException(message, exception);
-            }
-
-            GameData gameData = new GameData(
-                    storedGameId,
-                    whiteUsername,
-                    blackUsername,
-                    gameName,
-                    deserializedGame
-            );
-
+            GameData gameData = readGameFromResultSet(resultSet);
             return gameData;
         } catch (Exception exception) {
             String message = "failed to get game from database";
@@ -84,20 +64,112 @@ public class MySqlDataAccess implements DataAccess {
 
     @Override
     public GameData createGame(GameData game) throws DataAccessException {
-        String message = "createGame is not implemented for MySqlDataAccess";
-        throw new DataAccessException(message);
+        String sql = """
+            INSERT INTO games (game_id, white_username, black_username, game_name, game_state_json)
+            VALUES (?, ?, ?, ?, ?)
+            """;
+
+        try (var connection = DatabaseManager.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+
+            int gameId = game.gameID();
+            String whiteUsername = game.whiteUsername();
+            String blackUsername = game.blackUsername();
+            String gameName = game.gameName();
+            ChessGame chessGame = game.game();
+            String gameStateJson = gson.toJson(chessGame, ChessGame.class);
+
+            int gameIdParameterIndex = 1;
+            int whiteUsernameParameterIndex = 2;
+            int blackUsernameParameterIndex = 3;
+            int gameNameParameterIndex = 4;
+            int gameStateJsonParameterIndex = 5;
+
+            preparedStatement.setInt(gameIdParameterIndex, gameId);
+            preparedStatement.setString(whiteUsernameParameterIndex, whiteUsername);
+            preparedStatement.setString(blackUsernameParameterIndex, blackUsername);
+            preparedStatement.setString(gameNameParameterIndex, gameName);
+            preparedStatement.setString(gameStateJsonParameterIndex, gameStateJson);
+
+            int rowsUpdated = preparedStatement.executeUpdate();
+            boolean isMissingInsertedRow = rowsUpdated != 1;
+            if (isMissingInsertedRow) {
+                String message = "failed to insert game into database";
+                throw new DataAccessException(message);
+            }
+
+            return game;
+        } catch (Exception exception) {
+            String message = "failed to create game in database";
+            throw new DataAccessException(message, exception);
+        }
     }
 
     @Override
     public Collection<GameData> listGames() throws DataAccessException {
-        String message = "listGames is not implemented for MySqlDataAccess";
-        throw new DataAccessException(message);
+        String sql = """
+            SELECT game_id, white_username, black_username, game_name, game_state_json
+            FROM games
+            """;
+
+        try (var connection = DatabaseManager.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+            Collection<GameData> games = new ArrayList<>();
+
+            while (resultSet.next()) {
+                GameData gameData = readGameFromResultSet(resultSet);
+                games.add(gameData);
+            }
+
+            return games;
+        } catch (Exception exception) {
+            String message = "failed to list games from database";
+            throw new DataAccessException(message, exception);
+        }
     }
 
     @Override
     public void updateGame(GameData game) throws DataAccessException {
-        String message = "updateGame is not implemented for MySqlDataAccess";
-        throw new DataAccessException(message);
+        String sql = """
+            UPDATE games
+            SET white_username = ?, black_username = ?, game_name = ?, game_state_json = ?
+            WHERE game_id = ?
+            """;
+
+        try (var connection = DatabaseManager.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+
+            int gameId = game.gameID();
+            String whiteUsername = game.whiteUsername();
+            String blackUsername = game.blackUsername();
+            String gameName = game.gameName();
+            ChessGame chessGame = game.game();
+            String gameStateJson = gson.toJson(chessGame, ChessGame.class);
+
+            int whiteUsernameParameterIndex = 1;
+            int blackUsernameParameterIndex = 2;
+            int gameNameParameterIndex = 3;
+            int gameStateJsonParameterIndex = 4;
+            int gameIdParameterIndex = 5;
+
+            preparedStatement.setString(whiteUsernameParameterIndex, whiteUsername);
+            preparedStatement.setString(blackUsernameParameterIndex, blackUsername);
+            preparedStatement.setString(gameNameParameterIndex, gameName);
+            preparedStatement.setString(gameStateJsonParameterIndex, gameStateJson);
+            preparedStatement.setInt(gameIdParameterIndex, gameId);
+
+            int rowsUpdated = preparedStatement.executeUpdate();
+            boolean isMissingUpdatedRow = rowsUpdated != 1;
+            if (isMissingUpdatedRow) {
+                String message = "failed to update game in database";
+                throw new DataAccessException(message);
+            }
+        } catch (Exception exception) {
+            String message = "failed to update game in database";
+            throw new DataAccessException(message, exception);
+        }
     }
 
     @Override
@@ -116,5 +188,34 @@ public class MySqlDataAccess implements DataAccess {
     public void deleteAuth(String authToken) throws DataAccessException {
         String message = "deleteAuth is not implemented for MySqlDataAccess";
         throw new DataAccessException(message);
+    }
+
+    private GameData readGameFromResultSet(ResultSet resultSet) throws DataAccessException {
+        try {
+            int storedGameId = resultSet.getInt("game_id");
+            String whiteUsername = resultSet.getString("white_username");
+            String blackUsername = resultSet.getString("black_username");
+            String gameName = resultSet.getString("game_name");
+            String gameStateJson = resultSet.getString("game_state_json");
+
+            ChessGame deserializedGame;
+            try {
+                deserializedGame = gson.fromJson(gameStateJson, ChessGame.class);
+            } catch (JsonSyntaxException exception) {
+                String message = "corrupt game_state_json for game_id=" + storedGameId;
+                throw new DataAccessException(message, exception);
+            }
+
+            return new GameData(
+                    storedGameId,
+                    whiteUsername,
+                    blackUsername,
+                    gameName,
+                    deserializedGame
+            );
+        } catch (Exception exception) {
+            String message = "failed to read game from result set";
+            throw new DataAccessException(message, exception);
+        }
     }
 }
