8b0f183d78633d59f2ecfbdaf6d51f3b9e7fa983
Tyler Oustrich <oustrichtyler@gmail.com>
2026-04-06 20:50:23 -0600

    LoadGameMessage extending ServerMessage

--- shared/src/main/java/websocket/messages/LoadGameMessage.java
+++ shared/src/main/java/websocket/messages/LoadGameMessage.java
@@ -1,5 +1,15 @@
 package websocket.messages;
+import chess.ChessGame;
 
-public class LoadGameMessage {
-    
+public class LoadGameMessage extends ServerMessage {
+    private final ChessGame game;
+
+    public LoadGameMessage(ChessGame game) {
+        super(ServerMessageType.LOAD_GAME);
+        this.game = game;
+    }
+
+    public ChessGame getGame() {
+        return game;
+    }
 }
