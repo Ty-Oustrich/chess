b1576b33146deabf26a7c57884c6611e6f5f8135
Tyler Oustrich <oustrichtyler@gmail.com>
2026-04-09 19:23:17 -0600

    game handler imports and stubs ;)

--- client/src/main/java/client/GameHandler.java
+++ client/src/main/java/client/GameHandler.java
@@ -1,5 +1,14 @@
 package client;
 
-public class GameHandler {
-    
+import websocket.messages.LoadGameMessage;
+import websocket.messages.NotificationMessage;
+import websocket.messages.ErrorMessage;
+
+
+public interface GameHandler {
+    void onLoadGame(LoadGameMessage message);
+
+    void onError(ErrorMessage message);
+
+    void onNotification(NotificationMessage message);
 }
